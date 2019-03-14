295dc01b810785c2df00164bb9ee8799b1fa2ee8
Steve R <steven@jongensvantechniek.nl>
2019-03-14 14:04:29 +0100

    Tweak multi build some more

--- auth/src/test/kotlin/com/oceanpremium/api/controller/AuthPresenterTest.kt
+++ auth/src/test/kotlin/com/oceanpremium/api/controller/AuthPresenterTest.kt
@@ -2,8 +2,8 @@ package com.oceanpremium.api.controller
 
 import com.oceanpremium.api.ApiDriver
 import com.oceanpremium.api.model.Response
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
+import junit.framework.TestCase.assertEquals
+import org.hibernate.validator.internal.util.Contracts.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
