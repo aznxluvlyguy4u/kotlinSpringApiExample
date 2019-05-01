cfd84b0eb2c6a43abd595620c971fb3cbdf30eab
Steve R <steven@jongensvantechniek.nl>
2019-05-01 10:31:08 +0200

    Complete cleanup, prepare base for products endpoint

--- auth/src/main/kotlin/com/oceanpremium/api/auth/controller/AuthController.kt
+++ auth/src/main/kotlin/com/oceanpremium/api/auth/controller/AuthController.kt
@@ -1,88 +1,31 @@
 package com.oceanpremium.api.auth.controller
 
-import com.oceanpremium.api.auth.usecase.CreateAuthUseCase
-import com.oceanpremium.api.auth.usecase.DeleteAuthUseCase
-import com.oceanpremium.api.auth.usecase.RetrieveAuthUseCase
-import com.oceanpremium.api.auth.usecase.UpdateAuthUseCase
-import com.oceanpremium.api.core.model.Response
-import com.oceanpremium.api.core.model.User
+import com.oceanpremium.api.core.util.ObjectMapperConfig
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.core.io.ResourceLoader
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.time.LocalDate
-import java.util.*
 
 @RestController
 @RequestMapping("api/v1/auth")
-class AuthController(@Autowired private val createAuthUseCase: CreateAuthUseCase,
-                     @Autowired private val retrieveAuthUser: RetrieveAuthUseCase,
-                     @Autowired private val updateAuthUseCase: UpdateAuthUseCase,
-                     @Autowired private val deleteAuthUseCase: DeleteAuthUseCase) {
+class AuthController(@Autowired private val resourceLoader: ResourceLoader) {
+    private val API_DOC_FILE_SWAGGER = "openapi.json"
 
     companion object {
         private val logger = LoggerFactory.getLogger(this::class.java)
+        private val mapper = ObjectMapperConfig.mapper
     }
 
-    /***
-     * Convenience endpoint to check if the endpoint exists and for CloudWatch to keep the lambda function warm,
-     * by calling at timed intervals, thus minimizes cold starts to a certain point, as server-less containers are disposed
-     * after an arbitrary period.
-     */
-    @RequestMapping(method = [RequestMethod.HEAD])
+    @RequestMapping("docs")
     @ResponseBody
-    fun existsAuth(): Response {
-        logger.debug("HEAD auth")
+    fun getApiDocs(): ResponseEntity<Any?> {
+        logger.debug("GET auth api docs")
 
-        return Response(true, null)
-    }
-
-    @PostMapping
-    @ResponseBody
-    fun createAuth(@RequestBody user: User): Response {
-        logger.debug("POST auth")
-        val token = createAuthUseCase.execute(user)
-
-        return Response(true, token)
-    }
-
-    @GetMapping
-    @ResponseBody
-    fun getAuths(@RequestParam(value = "date", required = false)
-                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) date: LocalDate): Response {
-        logger.debug("GET auths")
-        val user = retrieveAuthUser.execute(User("foobar", "foobar@example.com"))
-
-        return Response(true, "getAuths with params: $date, result: $user")
-    }
-
-    @RequestMapping("/{id}")
-    @ResponseBody
-    fun getAuthById(@PathVariable("id") id: Int): Response {
-        logger.debug("GET authById")
-
-        retrieveAuthUser.execute(User("foobar", "foobar@example.com"))
-
-        return Response(true, "getAuths with id: $id")
-    }
-
-    @PutMapping("/{id}")
-    @ResponseBody
-    fun updateAuthById(@PathVariable("id") id: Int, @RequestBody user: User): Response {
-        logger.debug("PUT authById")
-
-        updateAuthUseCase.execute(User("foobar", "foobar@example.com"))
-
-        return Response(true, "updateAuthById: " + id + "Update, " + user.name + "!")
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseBody
-    fun deleteAuthById(@PathVariable("id") id: Int): Response {
-        logger.debug("DELETE authById")
-
-        deleteAuthUseCase.execute(UUID.randomUUID())
-
-        return Response(deleteAuthUseCase.execute(UUID.randomUUID()), "deleteAuthById: $id")
+        return ResponseEntity(
+            mapper.readTree(resourceLoader.getResource("classpath:$API_DOC_FILE_SWAGGER").file),
+            HttpStatus.OK
+        )
     }
 }
--- auth/src/test/kotlin/com/oceanpremium/api/auth/AuthControllerTest.kt
+++ auth/src/test/kotlin/com/oceanpremium/api/auth/AuthControllerTest.kt
@@ -1,19 +1,11 @@
 package com.oceanpremium.api.auth
 
 import com.oceanpremium.api.core.util.ObjectMapperConfig
-import com.oceanpremium.api.core.model.Response
-import com.oceanpremium.api.core.model.Token
-import com.oceanpremium.api.core.model.User
-import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.*
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.test.context.junit4.SpringRunner
-import java.time.LocalDateTime
-import java.time.ZoneOffset
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,103 +17,4 @@ class AuthControllerTest {
 
     @Autowired
     val restTemplate: TestRestTemplate? = null
-
-    /**
-     * Create auth.
-     */
-    @Test
-    fun createAuthTest() {
-        val headers = HttpHeaders()
-        headers.contentType = MediaType.APPLICATION_JSON
-
-        val mockedUser = User("mockedUser", "mocked@address.com")
-
-        val mockedToken = Token(
-            "access-token-test",
-            "refresh-token-test",
-            LocalDateTime.now().atZone(ZoneOffset.UTC)?.toInstant()?.toEpochMilli()!!,
-            mockedUser.emailAddress!!
-        )
-
-        val entity = HttpEntity(mapper.writeValueAsString(mockedUser), headers)
-
-        val response = restTemplate?.postForEntity(
-            endpoint,
-            entity,
-            Response::class.java
-        )
-
-        assertThat(response?.statusCodeValue).isEqualTo(HttpStatus.OK.value()) // Needs to be 201 CREATED
-
-        val responseBody = response?.body as Response
-        val message = responseBody.message as Map<*,*>
-
-        val token = mapper.convertValue(message, Token::class.java)
-
-        assertThat(token.accessToken).isEqualTo(mockedToken.accessToken)
-        assertThat(token.refreshToken).isEqualTo(mockedToken.refreshToken)
-        assertThat(token.emailAddress).isEqualTo(mockedToken.emailAddress)
-    }
-
-    /**
-     * Get auth by date.
-     */
-    @Test
-    fun getAuthByDateTest() {
-        val date = "2001-01-01"
-        val dateQueryParameter = "date"
-
-        val response = restTemplate?.getForEntity("$endpoint?$dateQueryParameter=$date", Response::class.java)
-        assertThat(response?.statusCodeValue).isEqualTo(HttpStatus.OK.value())
-
-        val data = response?.body as Response
-        assertThat(data.message).isNotNull
-    }
-
-    /**
-     * Get auth by id.
-     */
-    @Test
-    fun getAuthByIdTest() {
-        val id = "123"
-
-        val response = restTemplate?.getForEntity("$endpoint/$id", Response::class.java)
-        assertThat(response?.statusCodeValue).isEqualTo(HttpStatus.OK.value())
-
-        val data = response?.body as Response
-        assertThat(data.message).isEqualTo("getAuths with id: $id")
-    }
-
-    /**
-     * Update auth.
-     */
-    @Test
-    fun updateAuthByIdTest() {
-        val id = "123"
-
-        val headers = HttpHeaders()
-        headers.contentType = MediaType.APPLICATION_JSON
-
-        val mockedUser = User("mockedUser", "mocked@address.com")
-
-        val entity = HttpEntity(mapper.writeValueAsString(mockedUser), headers)
-        val response = restTemplate?.exchange("$endpoint/$id", HttpMethod.PUT, entity, Response::class.java, id)
-        assertThat(response?.statusCodeValue).isEqualTo(HttpStatus.OK.value())
-
-        val data = response?.body as Response
-        assertThat(data.message).isEqualTo("updateAuthById: 123Update, ${mockedUser.name}!")
-    }
-
-    /**
-     * Delete auth.
-     */
-    @Test
-    fun deleteAuthByIdTest() {
-        val id = "123"
-        val response = restTemplate?.exchange("$endpoint/$id", HttpMethod.DELETE, null, Response::class.java, id)
-        assertThat(response?.statusCodeValue).isEqualTo(HttpStatus.OK.value())
-
-        val data = response?.body as Response
-        assertThat(data.success).isEqualTo(true)
-    }
 }
