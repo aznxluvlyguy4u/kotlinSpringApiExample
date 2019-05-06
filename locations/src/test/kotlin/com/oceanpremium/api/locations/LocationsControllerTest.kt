6f43eaba3b1a89cfdf8c234e247a57ef35f4249d
Steve R <steven@jongensvantechniek.nl>
2019-05-06 14:33:29 +0200

    Implement functional integration test for locations endpoint

---
+++ locations/src/test/kotlin/com/oceanpremium/api/locations/LocationsControllerTest.kt
@@ -0,0 +1,44 @@
+package com.oceanpremium.api.locations
+
+import com.oceanpremium.api.core.model.Response
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class LocationsControllerTest {
+
+    @Autowired
+    val restTemplate: TestRestTemplate? = null
+
+    companion object {
+        private val endpoint = "/api/v1/locations"
+    }
+
+    /**
+     * Get location API docs.
+     */
+    @Test
+    fun testGetProductApiDocs() {
+        val response = restTemplate?.getForEntity("$endpoint/docs", Response::class.java)
+        assertThat(response?.statusCodeValue).isEqualTo(HttpStatus.OK.value())
+    }
+
+    /**
+     * Retrieve locations.
+     */
+    @Test
+    fun testGetLocations() {
+
+        val response = restTemplate?.getForEntity(endpoint, Response::class.java)
+
+        // Assert that the HTTP response code is OK
+        assertThat(response?.statusCodeValue).isEqualTo(HttpStatus.OK.value())
+    }
+}
