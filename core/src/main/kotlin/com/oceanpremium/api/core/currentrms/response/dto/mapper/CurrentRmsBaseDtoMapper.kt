90a139f2dfba2d169f3fff411d21e3e2e1805686
Yubin Chen <yubin@jongensvantechniek.nl>
2019-05-24 17:10:15 +0200

    Change meta type from any to MetaDto

--- core/src/main/kotlin/com/oceanpremium/api/core/currentrms/response/dto/mapper/CurrentRmsBaseDtoMapper.kt
+++ core/src/main/kotlin/com/oceanpremium/api/core/currentrms/response/dto/mapper/CurrentRmsBaseDtoMapper.kt
@@ -1,9 +1,10 @@
 package com.oceanpremium.api.core.currentrms.response.dto.mapper
 
+import com.oceanpremium.api.core.currentrms.response.dto.product.MetaDto
 import org.springframework.http.HttpStatus
 
 abstract class CurrentRmsBaseDtoMapper(code: Int) {
     var httpStatus: HttpStatus = HttpStatus.valueOf(code)
     var data: Any? = null
-    var meta: Any? = null
+    var meta: MetaDto? = null
 }
