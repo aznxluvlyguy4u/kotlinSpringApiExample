d4d574c931127fa543c6fbf0204e4d651d110483
Steve R <steven@jongensvantechniek.nl>
2019-06-10 21:05:50 +0200

    Move appliction beans to seperate config

---
+++ core/src/main/kotlin/com/oceanpremium/api/core/config/CorsConfig.kt
@@ -0,0 +1,31 @@
+package com.oceanpremium.api.core.config
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun simpleCorsFilter(): FilterRegistrationBean<CorsFilter> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.allowedOrigins = listOf("*")
+        config.allowedMethods = listOf("POST", "GET", "PUT", "DELETE")
+        config.allowedHeaders = listOf("*")
+        config.maxAge = 3600
+
+        source.registerCorsConfiguration("/**", config)
+
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+
+        return bean
+    }
+}
