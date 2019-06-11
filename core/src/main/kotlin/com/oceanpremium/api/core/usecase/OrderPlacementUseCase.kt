d6ed770b71218afb9a5394999d90d7c61a40c442
Steve R <steven@jongensvantechniek.nl>
2019-06-11 08:48:34 +0200

    Implement Order placement usecase

--- core/src/main/kotlin/com/oceanpremium/api/core/usecase/OrderPlacementUseCase.kt
+++ core/src/main/kotlin/com/oceanpremium/api/core/usecase/OrderPlacementUseCase.kt
@@ -1,13 +1,34 @@
 package com.oceanpremium.api.core.usecase
 
-import com.oceanpremium.api.core.model.Order
+import com.oceanpremium.api.core.model.OrderDto
+import org.springframework.beans.factory.annotation.Autowired
 
 interface OrderPlacementUseCase {
-    fun execute(order: Order): Order
+    fun execute(order: OrderDto): OrderDto
 }
 
-class OrderPlacementUseCaseImpl : OrderPlacementUseCase {
-    override fun execute(order: Order): Order {
+class OrderPlacementUseCaseImpl (
+    @Autowired private val checkProductBatchAvailabilityUseCase: CheckProductBatchAvailabilityUseCase,
+    @Autowired private val sendEmailUseCase: SendEmailUseCase) : OrderPlacementUseCase {
+
+    override fun execute(order: OrderDto): OrderDto {
+        order.products = checkProductBatchAvailabilityUseCase.execute(order.products)
+        var subTotal = 0.0
+
+        order.products.forEach { productItem ->
+            val totalPriceUnitPriceProduct = productItem.totalPrice!!.toDouble()
+            subTotal += totalPriceUnitPriceProduct
+
+            productItem.accessories?.forEach { accessoryItem ->
+                val totalPricePerUnitAccessory = accessoryItem.totalPrice!!.toDouble()
+                subTotal += totalPricePerUnitAccessory
+            }
+        }
+
+        order.totalPrice = "%.2f".format(subTotal)
+
+        sendEmailUseCase.execute(order)
+
         return order
     }
 }
