1c372941cf58e54cbab8f25e1a56d433f6e72502
Steve R <steven@jongensvantechniek.nl>
2019-06-11 23:24:45 +0200

    Implement AddressDto

--- core/src/main/kotlin/com/oceanpremium/api/core/model/ContactDetailsDto.kt
+++ core/src/main/kotlin/com/oceanpremium/api/core/model/ContactDetailsDto.kt
@@ -3,13 +3,22 @@ package com.oceanpremium.api.core.model
 import com.fasterxml.jackson.annotation.JsonInclude
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+class AddressDto(
+    var streetName: String? = null,
+    var streetNumber: String? = null,
+    var postalCode: String? = null,
+    var city: String? = null,
+    var country: String? = null) {
+    var streetNumberBlock: String? = null
+}
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class ContactDetailsDto(
     val firstName: String,
     val surName: String,
     val emailAddress: String,
     val phoneNumber: String,
-    val country: String,
-    val city: String
+    var address: AddressDto = AddressDto()
 ) {
     var fullName: String? = null
 
