db2d97033a531f3c967fc08b02f2030303201c04
Steve R <steven@jongensvantechniek.nl>
2019-06-26 22:40:00 +0200

    Remove obsolete imports

--- core/src/main/kotlin/com/oceanpremium/api/core/model/RentalPeriod.kt
+++ core/src/main/kotlin/com/oceanpremium/api/core/model/RentalPeriod.kt
@@ -3,8 +3,6 @@ package com.oceanpremium.api.core.model
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import com.oceanpremium.api.core.config.CurrentRmsCustomDateSerializer
-import com.oceanpremium.api.core.config.CurrentRmsLocalDateDeserializer
 import com.oceanpremium.api.core.config.CustomDateSerializer
 import com.oceanpremium.api.core.config.LocalDateDeserializer
 import org.joda.time.DateTime
