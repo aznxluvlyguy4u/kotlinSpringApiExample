6e99076b47ca5fa0092ba84a4a10dc0cce844ce1
Steve R <steven@jongensvantechniek.nl>
2019-06-27 11:17:15 +0200

    Fix type-o in role name

--- core/src/main/kotlin/com/oceanpremium/api/core/enum/ClientRoleType.kt
+++ core/src/main/kotlin/com/oceanpremium/api/core/enum/ClientRoleType.kt
@@ -1,7 +1,7 @@
 package com.oceanpremium.api.core.enum
 
 enum class ClientRoleType(val type: String) {
-    CHARGER_BROKEN_OR_MANAGER("CHARGER BROKEN / MANAGER"),
-    CAPTAIN_OR_CREW("CAPTAIN_/ CREW"),
+    CHARTER_BROKER_OR_MANAGER("CHARTER BROKER OR MANAGER"),
+    CAPTAIN_OR_CREW("CAPTAIN OR CREW"),
     OTHER("OTHER")
 }
