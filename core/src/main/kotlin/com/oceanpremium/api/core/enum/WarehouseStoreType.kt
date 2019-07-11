a0daad2742cbd45ece6542c8b8ab4f9b2049e169
Steve R <steven@jongensvantechniek.nl>
2019-07-11 20:39:47 +0200

    Setup enum for the different store types

---
+++ core/src/main/kotlin/com/oceanpremium/api/core/enum/WarehouseStoreType.kt
@@ -0,0 +1,10 @@
+package com.oceanpremium.api.core.enum
+
+
+enum class WarehouseStoreType(val type: String) {
+    NATIVE("Native"),
+    ALTERNATIVE("Alternative"),
+    GRAY("Gray"),
+    NEW_ITEMS("New Items"),
+    NONE("None")
+}
