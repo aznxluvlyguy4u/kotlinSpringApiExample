f699f994fb2e77b547d1659f7cc3e0b894a69d35
Steve R <steven@jongensvantechniek.nl>
2019-07-12 15:23:59 +0200

    Match contact details order form for role type

--- core/src/main/kotlin/com/oceanpremium/api/core/model/ContactDetailsDto.kt
+++ core/src/main/kotlin/com/oceanpremium/api/core/model/ContactDetailsDto.kt
@@ -16,7 +16,7 @@ class AddressDto(
 @JsonInclude(JsonInclude.Include.NON_NULL)
 
 class ContactDetailsDto(
-    var role: ClientRoleType,
+    var type: ClientRoleType?,
     val firstName: String,
     val surName: String,
     val emailAddress: String,
